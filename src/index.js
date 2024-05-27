10ba715542b8e0eed813540d2df58395d552325e
= <=>
2024-05-27 16:35:38 +0530

    utils written

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config({path : "./env"})
-connectDB();
+
+ 
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000 , ()=>{
+    console.log(`Server is running on PORT : ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log(`MongoDB connection failed :  ${err}` )
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+
+// 1st method
+const asyncHandeler  = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err))
+    }
+}
+
+export default asyncHandeler;
+
+//2nd method -> using higher order functions
+
+// const asyncHandeler = (fn) => {
+//     (req,res,next)=>{
+//         try {
+            
+//         } catch (error) {
+//            res.status(error.code || 500).json({
+//             success  : false,
+//             message : error.message
+//            })
+//         }
+//     }
+// }
